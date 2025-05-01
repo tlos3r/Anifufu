d02c7e9184128904064e4eaf09a43c3256003995
Fu <isla17092002@gmail.com>
2025-05-01 15:13:56 +0700

    update zoro api after 2 years :bug:

--- src/page/home/Home.tsx
+++ src/page/home/Home.tsx
@@ -1,6 +1,5 @@
 import Anime from "../../component/anime/Anime";
 import Slide from "../../component/slide/Slide";
-import Test from "../../component/test";
 function Home() {
     return (
         <div className="container mt-10">
